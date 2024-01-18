fcbdb0a5ba6d302471ceb91f96502837da134bbe
chimobi Jeremaih <chimobijeremaih@gmail.com>
2024-01-18 12:24:29 +0100

    first commit

---
+++ calculator.js
@@ -0,0 +1,22 @@
+// let display_screen = document.querySelector(".display-screen input");
+// const buttons = document.querySelectorAll("button"); 
+
+
+// console.log(buttons)
+
+// buttons.forEach(btn => {
+//     btn.addEventListener("click", () => {
+//         if(btn.className === "numbers" || btn.className === "operators"){
+//             display_screen.value += btn.innerHTML
+//         }else if(btn.className === "clear"){
+//             display_screen.value = ""
+//         }else if (btn.className === "backspace"){
+//             let string = display_screen.value.toString()
+//             display_screen.value = string.substr(0, string.length - 1)
+//         }
+//     }); 
+    
+// })
+
+
+
